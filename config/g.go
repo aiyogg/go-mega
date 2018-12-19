72a34175c23f755454613e229169e68730463d11
Chuck <tengchenc@gmail.com>
2018-12-19 23:33:18 +0800

    ⛏ add getcongi func

--- config/g.go
+++ config/g.go
@@ -1,9 +1,25 @@
 package config
 
-func init() {
+import (
+	"fmt"
+	"github.com/spf13/viper"
+)
 
+func init() {
+	projectName := "go-mega"
+	getConfig(projectName)
 }
 
 func getConfig(projectName string) {
+	viper.SetConfigName("config")
 
+	viper.AddConfigPath(".")
+	viper.AddConfigPath(fmt.Sprintf("$HOME/.%s", projectName))
+	viper.AddConfigPath(fmt.Sprintf("/data/docker/config/%s", projectName))
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("Fatal error config file: %s", err))
+	}
 }
+
