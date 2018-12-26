f825d3a916b650d8716446d94f91b1bf52f1f0f9
TuringChen <TuringChen@upchina.com>
2018-12-26 18:14:49 +0800

    ✨  add session

--- controller/g.go
+++ controller/g.go
@@ -1,14 +1,21 @@
 package controller
 
-import "html/template"
+import (
+	"github.com/gorilla/sessions"
+	"html/template"
+)
 
 var (
 	homeController home
 	templates      map[string]*template.Template
+	sessionName    string
+	store          *sessions.CookieStore
 )
 
 func init() {
 	templates = PopulateTemplates()
+	store = sessions.NewCookieStore([]byte("something-very-secret"))
+	sessionName = "go-mega"
 }
 
 // Startup func
